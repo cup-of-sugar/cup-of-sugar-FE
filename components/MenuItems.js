9bc8beb292bad5473d174b87d5d548a5cbc3dc6f
Carla Geglio <carla.geglio@gmail.com>
2020-04-04 16:32:50 -0600

    Create menu items component

---
+++ components/MenuItems.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
+import Colors from '../constants/Colors';
+import { useNavigation } from '@react-navigation/native';
+
+export default function MenuItems() {
+   const navigation = useNavigation();
+
+   return (
+     <View style={styles.drawerItems}>
+       <TouchableOpacity onPress={() => navigation.navigate('LinksScreen')}><Text style={styles.drawerText}>Go to Lending</Text></TouchableOpacity>
+       <TouchableOpacity onPress={() => navigation.navigate('LinksScreen')}><Text style={styles.drawerText}>View My Requests</Text></TouchableOpacity>
+       <TouchableOpacity onPress={() => navigation.navigate('LinksScreen')}><Text style={styles.drawerText}>Logout</Text></TouchableOpacity>
+     </View>
+   );
+ }
+
+ const styles = StyleSheet.create({
+   drawerItems: {
+     backgroundColor: Colors.lightBlue,
+     height: 300,
+     padding: 20,
+     width: 300,
+   },
+   drawerText: {
+     color: '#fff',
+     fontSize: 25,
+     fontWeight: 'bold',
+     padding: 15
+   },
+ });
