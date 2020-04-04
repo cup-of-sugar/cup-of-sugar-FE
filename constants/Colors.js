9754c4a052fbe1083e14ae765947848bed1b4fe8
Carla Geglio <carla.geglio@gmail.com>
2020-04-04 16:33:17 -0600

    Create variables for app colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -2,6 +2,9 @@ const tintColor = '#0C94E1';
 
 export default {
   tintColor,
+  darkBlue: '#385A94',
+  lightBlue: '#0C94E1',
+  aqua: '#0CE1C3',
   tabIconDefault: '#ccc',
   tabIconSelected: tintColor,
   tabBar: '#fefefe',
