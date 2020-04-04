f6442e677129ca88d84efa698bedf17d6eefa4c9
Carla Geglio <carla.geglio@gmail.com>
2020-04-04 16:34:28 -0600

    Remove unused styling

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,19 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import HomeForm from '../components/HomeForm'
-import ErrorBoundary from '../components/ErrorBoundary'
+import HomeForm from '../components/HomeForm';
+import ErrorBoundary from '../components/ErrorBoundary';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { MonoText } from '../components/StyledText';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function HomeScreen() {
+
   return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <Text style={styles.getStartedText}> What would you like to borrow today?</Text>
+          <Text style={styles.welcomeText}> What would you like to borrow today?</Text>
           <ErrorBoundary>
           <HomeForm />
           </ErrorBoundary>
@@ -20,7 +23,7 @@ export default function HomeScreen() {
 }
 
 HomeScreen.navigationOptions = {
-  header: null,
+  header: 'none',
 };
 
 const styles = StyleSheet.create({
@@ -28,142 +31,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
   contentContainer: {
+    justifyContent: 'center',
     paddingTop: 30,
   },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 25,
+  welcomeText: {
+    fontSize: 30,
     fontWeight: 'bold',
     color: 'black',
-    lineHeight: 24,
+    lineHeight: 35,
     margin: 10,
+    marginTop: 20,
     textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
-
-
-// <View style={styles.welcomeContainer}>
-//   <Image
-//     source={
-//       __DEV__
-//         ? require('../assets/images/robot-dev.png')
-//         : require('../assets/images/robot-prod.png')
-//     }
-//     style={styles.welcomeImage}
-//   />
-// </View>
-
-// <View style={styles.tabBarInfoContainer}>
-//   <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-//
-//   <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-//     <MonoText style={styles.codeHighlightText}>navigation/BottomTabNavigator.js</MonoText>
-//   </View>
-// </View>
-
-//
-// function DevelopmentModeNotice() {
-//   if (__DEV__) {
-//     const learnMoreButton = (
-//       <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-//         Learn more
-//       </Text>
-//     );
-//
-//     return (
-//       <Text style={styles.developmentModeText}>
-//         Development mode is enabled: your app will be slower but you can use useful development
-//         tools. {learnMoreButton}
-//       </Text>
-//     );
-//   } else {
-//     return (
-//       <Text style={styles.developmentModeText}>
-//         You are not in development mode: your app will run at full speed.
-//       </Text>
-//     );
-//   }
-// }
-//
-// function handleLearnMorePress() {
-//   WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-// }
-//
-// function handleHelpPress() {
-//   WebBrowser.openBrowserAsync(
-//     'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
-//   );
-// }
