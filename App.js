6aadf288d077f97eae0a10e6d09c2fd160016e5e
Carla Geglio <carla.geglio@gmail.com>
2020-04-04 16:27:38 -0600

    Add header options to homescreen

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { TouchableOpacity, Image, Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { SplashScreen } from 'expo';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import HomeScreen from './screens/HomeScreen'
+import LinksScreen from './screens/LinksScreen'
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 import useLinking from './navigation/useLinking';
+import cup from './assets/images/cup.png';
+import Colors from './constants/Colors';
+import MenuDrawer from './components/MenuDrawer'
 
 const Stack = createStackNavigator();
 
@@ -16,23 +21,21 @@ export default function App(props) {
   const [initialNavigationState, setInitialNavigationState] = React.useState();
   const containerRef = React.useRef();
   const { getInitialState } = useLinking(containerRef);
+  const [menuOpen, toggleMenuOpen] = React.useState(false)
 
-  // Load any resources or data that we need prior to rendering the app
+
   React.useEffect(() => {
     async function loadResourcesAndDataAsync() {
       try {
         SplashScreen.preventAutoHide();
 
-        // Load our initial navigation state
         setInitialNavigationState(await getInitialState());
 
-        // Load fonts
         await Font.loadAsync({
           ...Ionicons.font,
           'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
         });
       } catch (e) {
-        // We might want to provide this error information to an error reporting service
         console.warn(e);
       } finally {
         setLoadingComplete(true);
@@ -51,7 +54,32 @@ export default function App(props) {
         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
         <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
           <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
+            <Stack.Screen
+              name="Root"
+              component={HomeScreen}
+              options={{
+                headerStyle: {
+                  backgroundColor: Colors.darkBlue,
+                  height: 120,
+                },
+                headerTitleStyle: {
+                  color: 'white',
+                  fontSize: 25,
+                },
+                headerTitle: 'Cup of Sugar',
+                headerRight: () => (<Image
+                  source={cup}
+                  style={styles.logo} />
+                ),
+                headerLeft: () => (
+                  <MenuDrawer />
+                ),
+              }}
+            />
+            <Stack.Screen
+              name="LinksScreen"
+              component={LinksScreen}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </View>
@@ -59,9 +87,16 @@ export default function App(props) {
   }
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
   },
+  logo: {
+    margin: 15,
+    height: 42,
+    width: 57,
+  }
 });
