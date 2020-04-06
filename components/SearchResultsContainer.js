f48d0c862965a292c0b6321acc4bbbb60a0aed62
Carla Geglio <carla.geglio@gmail.com>
2020-04-06 15:48:30 -0600

    Change query to include category and item name

--- components/SearchResultsContainer.js
+++ components/SearchResultsContainer.js
@@ -5,31 +5,35 @@ import Colors from '../constants/Colors';
 import { gql } from "apollo-boost";
 import { useQuery } from '@apollo/react-hooks';
 
-const ITEMS = gql`
-  {
-    getAllItems {
-      name
-      quantity
-      measurement
-      description
-      category {
-        name
-      }
-    }
-  }
-`;
+export function SearchResultsContainer(props) {
 
-export function SearchResultsContainer() {
+    let category = props.items.category;
+    let item = props.items.itemName;
+
+    const ITEMS = gql`
+      {
+        getAllItemsByName(name: "${category}", items: "${item}") {
+          name
+          quantity
+          description
+          measurement
+          category {
+            name
+          }
+        }
+      }
+    `;
+
   const { loading, error, data } = useQuery(ITEMS);
 
-  if (loading) return <Text>Loading...</Text>
-  if (error) return <Text>Error</Text>
+  if (loading) return <Text style={styles.loadingText}>Loading...</Text>
+  if (error) return <Text style={styles.errorText}>No items found!</Text>
 
   if (data) return (
     <View style={styles.searchContainer}>
       <ScrollView style={styles.searchContainer} contentContainerStyle={styles.contentContainer}>
         <Text style={styles.resultsText}>Results:</Text>
-        {data.getAllItems.map(item => <SearchResult key={item.name} item={item} /> )}
+        {data.getAllItemsByName.map(item => <SearchResult key={item.name} item={item} /> )}
       </ScrollView>
     </View>
   )
@@ -53,5 +57,18 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: 'bold',
     color: 'black',
-  }
+  },
+  errorText: {
+    alignSelf: 'center',
+    color: 'red',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  loadingText: {
+    alignSelf: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  },
 });
