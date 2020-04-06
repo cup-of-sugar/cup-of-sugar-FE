f1a4246e10c6da7ecaa85a19e30f7f0f005c6a52
Carla Geglio <carla.geglio@gmail.com>
2020-04-06 15:49:27 -0600

    Make each item name lowercase before adding to state

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,29 +1,31 @@
-import * as WebBrowser from 'expo-web-browser';
-import * as React from 'react';
-import HomeForm from '../components/HomeForm';
-import ErrorBoundary from '../components/ErrorBoundary';
-import { SearchResultsContainer } from '../components/SearchResultsContainer';
+import * as WebBrowser from "expo-web-browser";
+import * as React from "react";
+import HomeForm from "../components/HomeForm";
+import ErrorBoundary from "../components/ErrorBoundary";
+import { SearchResultsContainer } from "../components/SearchResultsContainer";
 import {
   Image,
   Platform,
   StyleSheet,
   Text,
   TouchableOpacity,
-  View,
-} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import { MonoText } from '../components/StyledText';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+  View
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { MonoText } from "../components/StyledText";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default class HomeScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { searchQuery: { category: '', itemName: '' } };
+    this.state = { searchQuery: { category: "", itemName: "" } };
   }
 
   updateSearch = (itemName, category) => {
-    this.setState({ searchQuery: { category: category, itemName: itemName } });
+    this.setState({
+      searchQuery: { category: category, itemName: itemName.toLowerCase() }
+    });
   };
 
   render() {
@@ -34,7 +36,6 @@ export default class HomeScreen extends React.Component {
           contentContainerStyle={styles.contentContainer}
         >
           <Text style={styles.welcomeText}>
-            {' '}
             What would you like to borrow today?
           </Text>
           <ErrorBoundary>
@@ -52,26 +53,26 @@ export default class HomeScreen extends React.Component {
 }
 
 HomeScreen.navigationOptions = {
-  header: 'none',
+  header: "none"
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    overflow: 'scroll',
+    backgroundColor: "#fff",
+    overflow: "scroll"
   },
   contentContainer: {
-    justifyContent: 'center',
-    paddingTop: 30,
+    justifyContent: "center",
+    paddingTop: 30
   },
   welcomeText: {
     fontSize: 30,
-    fontWeight: 'bold',
-    color: 'black',
+    fontWeight: "bold",
+    color: "black",
     lineHeight: 35,
     margin: 10,
     marginTop: 20,
-    textAlign: 'center',
-  },
+    textAlign: "center"
+  }
 });
