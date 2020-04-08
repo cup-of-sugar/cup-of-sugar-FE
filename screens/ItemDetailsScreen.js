107393be172e3f77f837281756c60b58e2fdf786
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-04-08 16:12:08 -0600

    Request post (#26)
    * Fix conflict
    
    * Add check for Platform
    
    * Add local state
    
    * Reseperate inputs
    
    * Create conditional rendered component

--- screens/ItemDetailsScreen.js
+++ screens/ItemDetailsScreen.js
@@ -1,8 +1,8 @@
-import * as WebBrowser from "expo-web-browser";
-import * as React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import Colors from "../constants/Colors";
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import Colors from '../constants/Colors';
 
 import { gql } from "apollo-boost";
 import { useMutation } from "@apollo/react-hooks";
@@ -52,7 +52,7 @@ export default function ItemDetailsScreen(props) {
         <Text style={styles.itemInfo}>{item.category.name}</Text>
         <Text style={styles.itemInfoTitle}>Description:</Text>
         <Text style={styles.itemInfo}>
-          {item.description || "No description yet!"}
+          {item.description || 'No description yet!'}
         </Text>
       </View>
       {item.available ? (
@@ -85,15 +85,15 @@ export default function ItemDetailsScreen(props) {
 
 const styles = StyleSheet.create({
   container: {
-    alignContent: "center",
-    backgroundColor: "#fafafa",
+    alignContent: 'center',
+    backgroundColor: '#fafafa',
     flex: 1,
-    textAlign: "center"
+    textAlign: 'center',
   },
   contentContainer: {
-    justifyContent: "center",
+    justifyContent: 'center',
     padding: 15,
-    paddingTop: 50
+    paddingTop: 50,
   },
   borrowButton: {
     marginRight: 40,
@@ -104,35 +104,35 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.lightBlue,
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: "#fff"
+    borderColor: '#fff',
   },
   borrowButtonText: {
     fontSize: 25,
-    fontWeight: "bold",
-    color: "#fff",
-    textAlign: "center"
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
   },
   name: {
-    alignSelf: "center",
+    alignSelf: 'center',
     backgroundColor: Colors.aqua,
     fontSize: 35,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     height: 60,
     marginBottom: 25,
     paddingTop: 8,
-    textAlign: "center",
-    width: "100%"
+    textAlign: 'center',
+    width: '100%',
   },
   infoContainer: {
-    alignSelf: "center"
+    alignSelf: 'center',
   },
   itemInfo: {
     fontSize: 25,
-    marginBottom: 15
+    marginBottom: 15,
   },
   itemInfoTitle: {
     fontSize: 25,
-    fontWeight: "bold",
-    marginTop: 10
-  }
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
 });
--- screens/RequestScreen.js
+++ screens/RequestScreen.js
@@ -1,21 +1,55 @@
-import * as WebBrowser from "expo-web-browser";
-import * as React from "react";
-import { Component } from "react";
+import * as WebBrowser from 'expo-web-browser';
+import * as React from 'react';
+import { Component } from 'react';
 import {
   Picker,
   StyleSheet,
   Text,
   TextInput,
   View,
-  Button
-} from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+  Button,
+  Platform,
+} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export class RequestScreen extends Component {
+  constructor() {
+    super();
+    this.state = {
+      category: '',
+      name: '',
+      description: '',
+      amount: 0,
+      time: '',
+    };
+  }
+
+  handleChange = (event) => {
+    if (!event.target.name) {
+      event.target.name = 'category';
+    }
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
   render() {
+    const inputComponent = (
+      <TextInput
+        placeholder={this.state.category === 'food' ? 'How many?' : 'How long?'}
+        style={styles.input}
+        onChange={this.handleChange}
+        name="time"
+      ></TextInput>
+    );
+
+    const textComponent = (
+      <Text style={styles.label}>
+        {this.state.category === 'food' ? 'Amount' : 'Time'}
+      </Text>
+    );
+
     return (
       <View style={styles.container}>
-        <Text style={{ color: "#fff", fontSize: 24, fontWeight: "bold" }}>
+        <Text style={{ color: '#fff', fontSize: 24, fontWeight: 'bold' }}>
           What item are you looking for?
         </Text>
         <ScrollView
@@ -24,43 +58,76 @@ export class RequestScreen extends Component {
         >
           <Text style={styles.label}>Select a category*</Text>
           <Picker
-            selectedValue=""
-            style={{ height: 40, marginBottom: 8 }}
-            onValueChange={() => {}}
+            value=""
+            onChange={() => this.handleChange(event)}
+            style={{ marginBottom: 8, height: 40 }}
           >
-            <Picker.Item label="Garden" value="garden" />
-            <Picker.Item label="Pantry" value="pantry" />
-            <Picker.Item label="Cleaning" value="cleaning" />
+            <Picker.Item
+              label="Garden"
+              value="garden"
+              onChange={this.handleChange}
+            />
+            <Picker.Item
+              onChange={this.handleChange}
+              label="Food"
+              value="food"
+            />
+            <Picker.Item
+              onChange={this.handleChange}
+              label="Cleaning"
+              value="cleaning"
+            />
           </Picker>
           <Text style={styles.label}>Item name*</Text>
           <TextInput
             placeholder="Enter item name"
             style={styles.input}
-            onChangeText={() => {}}
-            value=""
+            name="name"
+            onChange={this.handleChange}
           ></TextInput>
           <Text style={styles.label}>Description*</Text>
           <TextInput
             placeholder="Enter description"
             style={styles.input}
-            onChangeText={() => {}}
-            value=""
+            onChange={this.handleChange}
+            name="description"
           ></TextInput>
-          <Text style={styles.label}>Amount</Text>
+          {textComponent}
+          {inputComponent}
+          {/* <Text style={styles.label}>Amount</Text>
           <TextInput
             placeholder="How many?"
             style={styles.input}
-            onChangeText={() => {}}
-            value=""
+            onChange={this.handleChange}
+            name="amount"
           ></TextInput>
           <Text style={styles.label}>Time</Text>
           <TextInput
             placeholder="How long?"
             style={styles.input}
-            onChangeText={() => {}}
-            value=""
-          ></TextInput>
-          <Button onPress={() => {}} title="Request" color="#385A94" />
+            onChange={this.handleChange}
+            name="time"
+          ></TextInput> */}
+          <Button
+            onPress={() => {}}
+            title="Request"
+            color="#385A94"
+            style={{
+              ...Platform.select({
+                ios: {
+                  backgroundColor: '#385A94',
+                  borderRadius: 8,
+                  color: '#fff',
+                },
+                android: {
+                  borderRadius: 8,
+                },
+                default: {
+                  borderRadius: 8,
+                },
+              }),
+            }}
+          />
         </ScrollView>
       </View>
     );
@@ -70,30 +137,31 @@ export class RequestScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0C94E1"
+    backgroundColor: '#0C94E1',
+    padding: 5,
   },
   contentContainer: {
-    justifyContent: "center",
-    paddingTop: 30
+    justifyContent: 'center',
+    paddingTop: 30,
   },
   input: {
     height: 40,
-    borderColor: "gray",
+    borderColor: 'gray',
     borderWidth: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     marginBottom: 8,
-    padding: 5
+    padding: 5,
   },
   label: {
-    color: "#fff"
+    color: '#fff',
   },
   welcomeText: {
     fontSize: 30,
-    fontWeight: "bold",
-    color: "black",
+    fontWeight: 'bold',
+    color: 'black',
     lineHeight: 35,
     margin: 10,
     marginTop: 20,
-    textAlign: "center"
-  }
+    textAlign: 'center',
+  },
 });
