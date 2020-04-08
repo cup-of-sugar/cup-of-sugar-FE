2c13c4a2c50fde146749b82ab06364788d738bcf
Carla Geglio <carla.geglio@gmail.com>
2020-04-08 16:23:51 -0600

    Pass close menu function as prop to menu items

--- components/MenuDrawer.js
+++ components/MenuDrawer.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
-import Drawer from 'react-native-drawer';
-import MenuItems from './MenuItems';
-import { withNavigation } from 'react-navigation';
-import menu from '../assets/images/menuicon.png';
-import menuClosed from '../assets/images/X.png';
+import React, { Component } from "react";
+import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
+import Drawer from "react-native-drawer";
+import MenuItems from "./MenuItems";
+import { withNavigation } from "react-navigation";
+import menu from "../assets/images/menuicon.png";
+import menuClosed from "../assets/images/X.png";
 
 export default class MenuDrawer extends Component {
   constructor() {
-    super()
-    this.state={ menuOpen: false }
+    super();
+    this.state = { menuOpen: false };
   }
+
+  closeMenu = () => {
+    this.setState({ menuOpen: false });
+  };
 
   render() {
     return (
       <View style={styles.drawerContainer}>
-        <TouchableOpacity onPress={() => this.setState({menuOpen: !this.state.menuOpen})}>
+        <TouchableOpacity
+          onPress={() => this.setState({ menuOpen: !this.state.menuOpen })}
+        >
           <Image
             source={this.state.menuOpen ? menuClosed : menu}
             style={styles.menu}
@@ -26,29 +32,33 @@ export default class MenuDrawer extends Component {
           type="static"
           openDrawerOffset={0.5}
           closedDrawerOffset={0}
-          content={this.state.menuOpen ? <MenuItems /> : null}
+          content={
+            this.state.menuOpen ? (
+              <MenuItems closeMenu={this.closeMenu} />
+            ) : null
+          }
           tapToClose={true}
           onClose={this.closeDrawer}
           styles={styles.drawer}
-          tweenEasing={'easeInOutQuad'}
+          tweenEasing={"easeInOutQuad"}
           tweenDuration={400}
         ></Drawer>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   drawerContainer: {
-    width: '190%',
+    width: "190%"
   },
   drawer: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff"
   },
   menu: {
     height: 30,
     margin: 20,
     marginTop: 23,
-    width: 40,
-  },
+    width: 40
+  }
 });
