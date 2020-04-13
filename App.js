41371dc6fac3aefc77067095c37fd600f1329571
Carla Geglio <carla.geglio@gmail.com>
2020-04-13 12:57:01 -0600

    Add menu item to navigate to item requests

--- App.js
+++ App.js
@@ -24,6 +24,7 @@ import PathScreen from "./screens/PathScreen";
 import LoginScreen from "./screens/LoginScreen";
 import MessagesScreen from "./screens/MessagesScreen";
 import MessageDetailsScreen from "./screens/MessageDetailsScreen";
+import OffersAndRequestsScreen from "./screens/OffersAndRequestsScreen";
 import useLinking from "./navigation/useLinking";
 import cup from "./assets/images/cup.png";
 import Colors from "./constants/Colors";
@@ -134,6 +135,23 @@ export default function App(props) {
                     backgroundColor: Colors.darkBlue,
                     height: 120
                   },
+                  headerTitleStyle: {
+                    color: "white",
+                    fontSize: 25
+                  },
+                  headerRight: () => <Image source={cup} style={styles.logo} />,
+                  headerTintColor: "white"
+                }}
+              />
+              <Stack.Screen
+                name="OffersAndRequests"
+                component={OffersAndRequestsScreen}
+                options={{
+                  headerStyle: {
+                    backgroundColor: Colors.darkBlue,
+                    height: 120
+                  },
+                  headerTitle: "",
                   headerTitleStyle: {
                     color: "white",
                     fontSize: 25
