69c0e130e8247aad76ec5ac36de6170d6d0415b7
Carla Geglio <carla.geglio@gmail.com>
2020-04-14 18:25:58 -0600

    Add mutation for sending a new message

--- screens/ComposeMessageScreen.js
+++ screens/ComposeMessageScreen.js
@@ -11,11 +11,45 @@ import {
 } from "react-native";
 import Colors from "../constants/Colors";
 import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
+import { useMutation } from "@apollo/react-hooks";
 import plane from "../assets/images/plane.png";
 
 export default function ComposeMessageScreen(props) {
-  return <ComposeForm action={props.action} navigation={props.navigation} />;
+  let title, body, recipientId, userId;
+
+  const NEW_MESSAGE = gql`
+    mutation SendMessage($title: String!, $body: String!, $recipientId: ID!) {
+      message: sendMessage(
+        input: {
+          title: $title
+          body: $body
+          userId: "1"
+          recipientId: $recipientId
+        }
+      ) {
+        id
+        title
+        body
+      }
+    }
+  `;
+
+  const [sendMessage] = useMutation(NEW_MESSAGE, {
+    variables: {
+      title,
+      body,
+      userId,
+      recipientId
+    }
+  });
+
+  return (
+    <ComposeForm
+      {...props}
+      navigation={props.navigation}
+      sendMessage={sendMessage}
+    />
+  );
 }
 
 export class ComposeForm extends React.Component {
@@ -35,7 +69,17 @@ export class ComposeForm extends React.Component {
   };
 
   sendMessage = () => {
-    this.setState({ sent: true });
+    this.props
+      .sendMessage({
+        variables: {
+          userId: this.props.userId,
+          recipientId: "4",
+          title: this.state.subject,
+          body: this.state.body
+        }
+      })
+      .then(() => this.setState({ sent: true }))
+      .catch(error => console.log(error));
   };
 
   render() {
