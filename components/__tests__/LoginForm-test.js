f8cf28568312d8971847f2b231059242878990a7
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-04-15 12:57:25 -0600

    Fix login test

--- components/__tests__/LoginForm-test.js
+++ components/__tests__/LoginForm-test.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { LoginFormClass } from "../LoginForm";
 
-import renderer from "react-test-renderer";
+import renderer, { act } from "react-test-renderer";
+import { MockedProvider } from "@apollo/react-testing";
 
-test("should render component", () => {
+it("should render component", async () => {
   const props = {
     navigation: { navigate: jest.fn() },
     userLogin: jest.fn(),
     action: "lend",
   };
-  const tree = renderer.create(<LoginFormClass {...props} />).toJSON();
+  let tree;
+  await act(async () => {
+    tree = await renderer.create(
+      <MockedProvider mocks={[]}>
+        <LoginFormClass {...props} />
+      </MockedProvider>
+    );
+  });
   expect(tree).toMatchSnapshot();
 });
