988d466b84969c85a35e529b3ae7c68b3bf9e737
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-04-15 10:51:41 -0600

    Fix tests for ComposeMessageScreen

--- screens/__tests__/ComposeMessageScreen-test.js
+++ screens/__tests__/ComposeMessageScreen-test.js
@@ -3,26 +3,30 @@ import ComposeMessageScreen from "../ComposeMessageScreen";
 import { MockedProvider } from "@apollo/react-testing";
 import ReactTestRenderer from "react-test-renderer";
 
-it("should render without error if on borrowing path", () => {
+it("should render without error if on borrowing path", async () => {
   const props = {
     navigation: { navigate: jest.fn() },
-    route: { params: { action: "borrow", userId: "4" } }
+    route: { params: { action: "borrow", userId: "4" } },
   };
-  ReactTestRenderer.create(
+  let tree = await ReactTestRenderer.create(
     <MockedProvider mocks={[]}>
       <ComposeMessageScreen {...props} />
     </MockedProvider>
   );
+
+  expect(tree).toMatchSnapshot();
 });
 
-it("should render without error if on lending path", () => {
+it("should render without error if on lending path", async () => {
   const props = {
     navigation: { navigate: jest.fn() },
-    route: { params: { action: "lend", userId: "4" } }
+    route: { params: { action: "lend", userId: "4" } },
   };
-  ReactTestRenderer.create(
+  let tree = await ReactTestRenderer.create(
     <MockedProvider mocks={[]}>
       <ComposeMessageScreen {...props} />
     </MockedProvider>
   );
+
+  expect(tree).toMatchSnapshot();
 });
