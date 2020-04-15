b24e31bbc58cd9b1ddc35bbd254531d0eca41ba7
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-04-15 09:44:52 -0600

    Fix testing for MenuItems

--- components/__tests__/MenuItems-test.js
+++ components/__tests__/MenuItems-test.js
@@ -1,14 +1,36 @@
-import React from 'react';
-import MenuItem from '../MenuDrawer';
+import React from "react";
+import MenuItems from "../MenuItems";
 
-import renderer from 'react-test-renderer';
-import { useNavigation } from '@react-navigation/native';
+import renderer from "react-test-renderer";
+import { MockedProvider } from "@apollo/react-testing";
 
-test('should render component', () => {
+it("should render component when action is lend", () => {
   const props = {
     closeMenu: jest.fn(),
-    action: 'lend',
+    action: "lend",
   };
-  const tree = renderer.create(<MenuItem {...props} />).toJSON();
+  const tree = renderer
+    .create(
+      <MockedProvider mocks={[]}>
+        <MenuItems {...props} />
+      </MockedProvider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("should render component when action is borrow", () => {
+  const props = {
+    closeMenu: jest.fn(),
+    action: "borrow",
+  };
+  const tree = renderer
+    .create(
+      <MockedProvider mocks={[]}>
+        <MenuItems {...props} />
+      </MockedProvider>
+    )
+    .toJSON();
+  console.log(tree);
   expect(tree).toMatchSnapshot();
 });
