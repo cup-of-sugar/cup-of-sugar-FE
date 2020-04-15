ff6bdafde994828bc878a1386db5acf4bb8128d3
Carla Geglio <carla.geglio@gmail.com>
2020-04-14 21:15:37 -0600

    Seperate outbox into new component with graphql query

---
+++ components/Outbox.js
@@ -0,0 +1,109 @@
+import React from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  Image
+} from "react-native";
+import { SearchResult } from "./SearchResult";
+import Colors from "../constants/Colors";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import { useNavigation } from "@react-navigation/native";
+import borrower from "../assets/images/borrower.png";
+import lender from "../assets/images/lender.png";
+
+export default function Outbox(props) {
+  const navigation = useNavigation();
+
+  const SENT_MESSAGES = gql`
+    {
+      userOutbox(userId: "1") {
+        title
+        body
+        recipient {
+          firstName
+          email
+        }
+      }
+    }
+  `;
+
+  let { loading, error, data } = useQuery(SENT_MESSAGES, {
+    fetchPolicy: "network-only"
+  });
+
+  if (loading) return <Text style={styles.loadingText}>Loading...</Text>;
+
+  if (error) return <Text style={styles.errorText}>No messages found!</Text>;
+
+  if (data) {
+    return data.userOutbox.length ? (
+      data.userOutbox.map(mail => {
+        return (
+          <TouchableOpacity
+            key={mail.title}
+            style={styles.message}
+            onPress={() =>
+              navigation.navigate("Message", {
+                message: mail,
+                userId: props.userId
+              })
+            }
+          >
+            <Text style={styles.messageText}>
+              To: {mail.recipient.firstName}
+            </Text>
+            <Image source={borrower} style={styles.icon} />
+          </TouchableOpacity>
+        );
+      })
+    ) : (
+      <Text style={styles.errorText}>No messages found!</Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  message: {
+    alignSelf: "center",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff",
+    flex: 0,
+    flexDirection: "row",
+    height: 70,
+    marginHorizontal: 40,
+    marginVertical: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 15,
+    width: 350
+  },
+  messageText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: Colors.darkBlue,
+    paddingRight: 10
+  },
+  icon: {
+    height: 25,
+    width: 115
+  },
+  loadingText: {
+    alignSelf: "center",
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 10
+  },
+  errorText: {
+    alignSelf: "center",
+    color: "red",
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 10,
+    marginTop: 50
+  }
+});
