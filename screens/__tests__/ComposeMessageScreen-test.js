c3ebc95897291b4fa4e578653b44d339cff27916
Carla Geglio <carla.geglio@gmail.com>
2020-04-14 22:52:39 -0600

    Update messages screen tests

--- screens/__tests__/ComposeMessageScreen-test.js
+++ screens/__tests__/ComposeMessageScreen-test.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { ComposeMessageScreen } from "../ComposeMessageScreen";
-import { create, act } from "react-test-renderer";
+import ComposeMessageScreen from "../ComposeMessageScreen";
 import { MockedProvider } from "@apollo/react-testing";
 import ReactTestRenderer from "react-test-renderer";
-import { gql } from "apollo-boost";
 
-it("should render without error", () => {
+it("should render without error if on borrowing path", () => {
   const props = {
     navigation: { navigate: jest.fn() },
     route: { params: { action: "borrow", userId: "4" } }
@@ -17,7 +15,7 @@ it("should render without error", () => {
   );
 });
 
-it("should render without error", () => {
+it("should render without error if on lending path", () => {
   const props = {
     navigation: { navigate: jest.fn() },
     route: { params: { action: "lend", userId: "4" } }
